e1ac0573664096887c02a1947f2bd28d67c32a69
Quintin Vizza <vizzaq@mail.sacredheart.edu>
2026-09-14 20:10:17 -0400

    test: cover shop title search

--- lib/shop/search.test.ts
+++ lib/shop/search.test.ts
@@ -1,9 +1,3 @@
 import { expect, it } from "vitest";
 import { matchesShopSearch } from "@/lib/shop/search";
-
-it("matches listing titles case-insensitively", () =>
-  expect([
-    matchesShopSearch("Monkey D. Luffy SEC", "luffy"),
-    matchesShopSearch("Roronoa Zoro SR", "luffy"),
-  ]).toEqual([true, false]),
-);
+it("matches listing titles case-insensitively", () => expect([matchesShopSearch("Monkey D. Luffy SEC", "luffy"), matchesShopSearch("Roronoa Zoro SR", "luffy")]).toEqual([true, false]));

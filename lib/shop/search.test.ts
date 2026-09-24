941ed66660fdc09c3e2a222a5696293097785fc4
Quintin Vizza <vizzaq@mail.sacredheart.edu>
2026-09-23 20:05:23 -0400

    test: verify shop metadata search

--- lib/shop/search.test.ts
+++ lib/shop/search.test.ts
@@ -1,4 +1,3 @@
 import { expect, it } from "vitest";
 import { matchesShopSearch } from "@/lib/shop/search";
-// @ts-expect-error extra listing details are not supported yet
 it("matches listing metadata case-insensitively", () => expect([matchesShopSearch("Monkey D. Luffy SEC", "romance dawn", "Romance Dawn", "SEC"), matchesShopSearch("Roronoa Zoro SR", "leader", "Romance Dawn", "Leader")]).toEqual([true, true]));

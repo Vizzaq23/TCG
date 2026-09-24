636b002f2e311664b7cfe2b518483cf89af460e5
Quintin Vizza <vizzaq@mail.sacredheart.edu>
2026-09-23 20:09:05 -0400

    test: cover search positive and negative cases

--- lib/shop/search.test.ts
+++ lib/shop/search.test.ts
@@ -1,3 +1,3 @@
 import { expect, it } from "vitest";
 import { matchesShopSearch } from "@/lib/shop/search";
-it("matches listing metadata case-insensitively", () => expect([matchesShopSearch("Monkey D. Luffy SEC", "romance dawn", "Romance Dawn", "SEC"), matchesShopSearch("Roronoa Zoro SR", "leader", "Romance Dawn", "Leader")]).toEqual([true, true]));
+it("matches listing metadata case-insensitively", () => expect([matchesShopSearch("Monkey D. Luffy SEC", "luffy", "Romance Dawn", "SEC"), matchesShopSearch("Monkey D. Luffy SEC", "romance dawn", "Romance Dawn", "SEC"), matchesShopSearch("Roronoa Zoro SR", "leader", "Romance Dawn", "Leader"), matchesShopSearch("Roronoa Zoro SR", "nami", "Romance Dawn", "Leader")]).toEqual([true, true, true, false]));
